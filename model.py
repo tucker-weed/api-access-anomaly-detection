c277526f1ac194d3c60d8e1d6fdea753b5b7ec43
tuckweed <tucker_weed@brown.edu>
2021-12-13 21:21:46 -0500

    [feat] completed model 2.0 and preprocessor 2.0

--- model.py
+++ model.py
@@ -5,6 +5,9 @@ import tensorflow as tf
 import numpy as np
 
 
+SHOW_EPOCH_TEST_BREAKDOWN = False
+
+
 class Model(tf.keras.Model):
     def __init__(self):
         """
@@ -12,30 +15,38 @@ class Model(tf.keras.Model):
         """
         super(Model, self).__init__()
 
-        self.batch_size = 100
+        self.batch_size = 50
         self.num_classes = 2
 
         # Hyperparameters
-        self.learning_rate = 0.001
-        self.num_epochs = 10
+        self.learning_rate = 0.0003
+        self.num_epochs = 7
 
         # Trainable parameters
         self.D1 = tf.keras.layers.Dense(self.batch_size, activation="relu")
-        self.D2 = tf.keras.layers.Dense(50, activation="relu")
-        self.D3 = tf.keras.layers.Dense(25, activation="relu")
-        self.D4 = tf.keras.layers.Dense(self.num_classes)
+        self.D2 = tf.keras.layers.Dense(25, activation="relu")
+        self.D3 = tf.keras.layers.Dense(20, activation="relu")
+        self.D4 = tf.keras.layers.Dense(15, activation="relu")
+        self.D5 = tf.keras.layers.Dense(10, activation="relu")
+        self.D6 = tf.keras.layers.Dense(5, activation="relu")
+        self.D7 = tf.keras.layers.Dense(self.num_classes)
+
+
 
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
 
-    def call(self, inputs, is_testing=False):
+    def call(self, inputs):
         """
         Runs a forward pass on an input batch of api data.
         """
         layer1_output = self.D1(inputs)
         layer2_output = self.D2(layer1_output)
         layer3_output = self.D3(layer2_output)
+        layer4_output = self.D4(layer3_output)
+        layer5_output = self.D5(layer4_output)
+        layer6_output = self.D6(layer5_output)
 
-        logits = self.D4(layer3_output)
+        logits = self.D7(layer6_output)
 
         return logits
 
@@ -69,34 +80,56 @@ def train(model, train_inputs, train_labels):
         steps += 1
         image = train_inputs[i:i + BATCH_SZ]
         label = train_labels[i:i + BATCH_SZ]
-        loss = None
         with tf.GradientTape() as tape:
             predictions = model.call(image)
             loss = model.loss(predictions, label)
 
         train_acc = model.accuracy(predictions, label)
-        print("Loss: {} | Accuracy on training set after {} steps: {}".format(str(loss.numpy())[: 6], steps, train_acc))
+        if steps % 50 == 0:
+            print("Loss: {} | Accuracy on training set after {} steps: {}".format(str(loss.numpy())[: 6], steps, train_acc))
 
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 
-def test(model, test_inputs, test_labels, setType):
+def test(model, test_inputs, test_labels):
     """
     Tests the model on the test inputs and labels.
     """
     BATCH_SZ = model.batch_size
     accs = []
+    inds = np.arange(0, np.shape(test_inputs)[0])
+    np.random.shuffle(inds)
+    test_inputs = test_inputs[inds]
+    test_labels = test_labels[inds]
 
     steps = 0
     for i in range(0, np.shape(test_inputs)[0], BATCH_SZ):
         steps += 1
+        outlier_acc = 0.0
+        normal_acc = 0.0
+        total_normal = 0
+        total_outlier = 0
         image = test_inputs[i:i + BATCH_SZ]
         label = test_labels[i:i + BATCH_SZ]
-        predictions = model.call(image, is_testing=True)
+        predictions = model.call(image)
         loss = model.loss(predictions, label)
         acc = model.accuracy(predictions, label)
-        print("Loss: {} | Accuracy on {} set after {} steps: {}".format(str(loss.numpy())[: 6], setType, steps, acc))
+        preds_correct = tf.equal(tf.argmax(predictions, 1), tf.argmax(label, 1))
+        for j in range(len(predictions)):
+            if int(tf.argmax(predictions[j])) == 1:
+                total_normal += 1
+                if bool(preds_correct[j]):
+                    normal_acc += 1.0
+            elif int(tf.argmax(predictions[j])) == 0:
+                total_outlier += 1
+                if bool(preds_correct[j]):
+                    outlier_acc += 1.0
+        if total_normal > 0 and total_outlier > 0:
+            normal_acc /= total_normal
+            outlier_acc /= total_outlier
+        print("Loss: {} | Accuracy on test set after {} steps: {}".format(str(loss.numpy())[: 6], steps, acc))
+        print("Correct classifications for --> NORMAL: {}, ANOMALY: {}".format(normal_acc, outlier_acc))
         accs.append(acc)
     return tf.reduce_mean(tf.convert_to_tensor(accs))
 
@@ -105,19 +138,20 @@ def main():
     """
     Executes training and testing steps.
     """
+    global SHOW_EPOCH_TEST_BREAKDOWN
     model = Model()
     (inp_train, lab_train, inp_test, lab_test) = get_data("./data/remaining_behavior_ext.csv")
 
     for epoch in range(model.num_epochs):
         print("\nEPOCH: {}\n".format(epoch + 1))
         train(model, inp_train, lab_train)
-        if epoch < model.num_epochs - 1:
+        if epoch < model.num_epochs - 1 and SHOW_EPOCH_TEST_BREAKDOWN:
             print("\nCURRENT TEST ACCURACY\n")
-            acc = test(model, inp_test, lab_test, setType="test")
+            acc = test(model, inp_test, lab_test)
             print("Aggregate accuracy: {}".format(acc))
 
     print("\nTEST SET\n")
-    acc = test(model, inp_test, lab_test, setType="test")
+    acc = test(model, inp_test, lab_test)
     print("\nFINAL TEST ACCURACY: {}\n".format(acc))
 
 
--- preprocess.py
+++ preprocess.py
@@ -3,32 +3,58 @@ import csv
 import random
 
 
+RANDOM_SEED = 1000
+
+
+def process_line_values(line, text_accum, max_val_tracker):
+    tup = \
+        float(line[2]), float(line[3]), float(line[4]), \
+        float(line[5]), float(line[9]), float(line[10]), \
+        float(line[11])
+    # These values are the relevant numeric data from row
+    val1, val2, val3, val4, val5, val6, val7 = tup
+    if val1 > max_val_tracker[0]:
+        max_val_tracker[0] = val1
+    if val2 > max_val_tracker[1]:
+        max_val_tracker[1] = val2
+    if val3 > max_val_tracker[2]:
+        max_val_tracker[2] = val3
+    if val4 > max_val_tracker[3]:
+        max_val_tracker[3] = val4
+    if val5 > max_val_tracker[4]:
+        max_val_tracker[4] = val5
+    if val6 > max_val_tracker[5]:
+        max_val_tracker[5] = val6
+    if val7 > max_val_tracker[6]:
+        max_val_tracker[6] = val7
+    text_accum.extend(tup)
+    data_label = line[8].lower()
+    return data_label
+
+
 def extract_data_suites(unprocessed):
+    global RANDOM_SEED
     text = []
     labels = []
     test_text = []
     test_labels = []
-    # Well shuffled data seems to perform better across runs, so x2 shuffle
-    random.shuffle(unprocessed)
-    random.shuffle(unprocessed)
-    split_len_test = int(len(unprocessed) * 0.1)
-    split_len_train = int(len(unprocessed) * 0.9)
+    split_len_test = int(len(unprocessed) * 0.15)
+    split_len_train = int(len(unprocessed) * 0.85)
     outlier_count_test = 0
     normal_count_test = 0
     outlier_count_train = 0
     normal_count_train = 0
+    max_val_tracker = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    # Well shuffled data seems to perform better across runs, so x2 shuffle
+    random.seed(RANDOM_SEED)
+    random.shuffle(unprocessed)
+    random.shuffle(unprocessed)
     for line in unprocessed:
         text_accum = []
         try:
-            text_accum.append(float(line[2]))
-            text_accum.append(float(line[3]))
-            text_accum.append(float(line[4]))
-            text_accum.append(float(line[5]))
-            text_accum.append(float(line[9]))
-            text_accum.append(float(line[10]))
-            text_accum.append(float(line[11]))
-            if line[8].lower() == "normal":
-                if normal_count_test < int(split_len_test * 0.9):
+            data_label = process_line_values(line, text_accum, max_val_tracker)
+            if data_label == "normal":
+                if normal_count_test < int(split_len_test * 0.5):
                     test_labels.append([0.0, 1.0])
                     test_text.append(text_accum)
                     normal_count_test += 1
@@ -36,8 +62,8 @@ def extract_data_suites(unprocessed):
                     labels.append([0.0, 1.0])
                     text.append(text_accum)
                     normal_count_train += 1
-            elif line[8].lower() == "outlier":
-                if outlier_count_test < int(split_len_test * 0.1):
+            elif data_label == "outlier":
+                if outlier_count_test < int(split_len_test * 0.5):
                     test_labels.append([1.0, 0.0])
                     test_text.append(text_accum)
                     outlier_count_test += 1
@@ -48,6 +74,13 @@ def extract_data_suites(unprocessed):
         except Exception as e:
             # Discard ambiguous data entries
             continue
+    # Normalize data
+    for row in text:
+        for i in range(len(row)):
+            row[i] /= max_val_tracker[i]
+    for row in test_text:
+        for i in range(len(row)):
+            row[i] /= max_val_tracker[i]
     return np.array(text), np.array(labels), np.array(test_text), np.array(test_labels)
 
 
